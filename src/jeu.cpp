696e7c64586c08f5e71a4d3201499f68f1b865da
RayanBlb <rayan.benlacheheb.cours@gmail.com>
2022-04-03 20:47:48 +0200

    Rayan - mode manuel terminé

--- src/jeu.cpp
+++ src/jeu.cpp
@@ -21,6 +21,7 @@ public:
   void mode_manuel_jeu();
   void select_mode_jeu(int mode_jeu);
   void deplacement_disque_jeu(int id_tour_e, int id_tour_s, void (jeu::*fonction_erreur)());
+  int check_win_jeu();
 
   // Getter et setter
   vector<tour> getTab_tour();
@@ -69,22 +70,18 @@ void jeu::deplacement_disque_jeu(int id_tour_e, int id_tour_s, void (jeu::*fonct
 {
 
   int result_e = 0;
-  int result_s = 0;
+  int result_s = -1;
 
   try
   {
     vector<disque> select_tour_e = this->tab_tour.at(id_tour_e - 1).getTab_disque();
     vector<disque> select_tour_s = this->tab_tour.at(id_tour_s - 1).getTab_disque();
 
-    for (int i = select_tour_e.size(); i = 0; i--) // Permet d'avoir la dernier case pleine
+    for (int i = select_tour_e.size() - 1; i >= 0; i--) // Permet d'avoir la dernier case pleine
     {
       if (select_tour_e.at(i).getTaille() != 0)
       {
         result_e = i;
-      }
-      else
-      {
-        result_e = -1;
       }
     }
 
@@ -93,10 +90,6 @@ void jeu::deplacement_disque_jeu(int id_tour_e, int id_tour_s, void (jeu::*fonct
       if (select_tour_s.at(z).getTaille() == 0)
       {
         result_s = z;
-      }
-      else
-      {
-        result_s = -1;
       }
     }
 
@@ -108,40 +101,61 @@ void jeu::deplacement_disque_jeu(int id_tour_e, int id_tour_s, void (jeu::*fonct
   }
   catch (const exception &e)
   {
-    cerr << "Error : Bad entry. "<< e.what() << endl;
+    cerr << "Error : Bad entry. " << e.what() << endl;
     (this->*fonction_erreur)();
   }
 }
 
 void jeu::mode_manuel_jeu()
 {
-  int id_tour_e = 0;
-  int id_tour_s = 0;
-
-  cout << "Quel tour ?" << endl;
-  cin >> id_tour_e;
-
-  cout << "Ou tour ?" << endl;
-  cin >> id_tour_s;
-
-  while (cin.fail())
+  do
   {
-    cin.clear();
-    cin.ignore(numeric_limits<streamsize>::max(),'\n');
-    cout << "Error : Bad entry."<< endl;
+    int id_tour_e = 0;
+    int id_tour_s = 0;
 
     cout << "Quel tour ?" << endl;
     cin >> id_tour_e;
 
     cout << "Ou tour ?" << endl;
     cin >> id_tour_s;
-  }
-  deplacement_disque_jeu(id_tour_e, id_tour_s,&jeu::mode_manuel_jeu);
+
+    while (cin.fail())
+    {
+      cin.clear();
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+      cout << "Error : Bad entry." << endl;
+
+      cout << "Quel tour ?" << endl;
+      cin >> id_tour_e;
+
+      cout << "Ou tour ?" << endl;
+      cin >> id_tour_s;
+    }
+    deplacement_disque_jeu(id_tour_e, id_tour_s, &jeu::mode_manuel_jeu);
+    affiche_jeu();
+  } while (check_win_jeu() == 0);
+  cout << "YOU WIN !!!" << endl;
 }
 
 void jeu::select_mode_jeu(int mode_jeu)
 {
   mode_manuel_jeu();
+}
+
+int jeu::check_win_jeu()
+{
+  int last = tab_tour.size();
+  int check = 1;
+  vector<disque> select_tour_last = this->tab_tour.at(2).getTab_disque();
+
+  for (int i = 0; i < select_tour_last.size(); i++)
+  {
+    if (select_tour_last.at(i).getTaille() != i + 1)
+    {
+      check = 0;
+    }
+  }
+  return check;
 }
 
 // Getter
--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,9 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     
-    /*
-    int mode_jeu = 0;
-
-    cout <<"TOUR DE HANOI LE JEU"<<endl;
-
-    cout << "Vous voulez jouer avec combien de disque ?" << endl;
-    cin >> nb_disque;
-
-    cout << "Mode de jeu ? ( 0 = mode manuel | 1 = mode automatique )" << endl;
-    cin >> mode_jeu;*/
-    
-    jeu jeu(3,9);
+    jeu jeu(3,3);
     jeu.affiche_jeu();
-
     jeu.select_mode_jeu(0);
-    jeu.affiche_jeu();
 
     return 0;
 }
